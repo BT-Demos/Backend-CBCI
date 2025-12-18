86ee1a4690a64b130b9173f14f26fa2bbff37ff9
Runxia Ye <rye@cloudbees.com>
2025-12-18 01:30:50 +0100

    [PMDEMO-4] Add Health Check controller backend

---
+++ src/main/java/com/in28minutes/unittesting/unittesting/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.in28minutes.unittesting.unittesting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Backend service is healthy - v6.0.0";
+    }
+}
